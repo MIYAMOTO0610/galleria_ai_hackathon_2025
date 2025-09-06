834f69003d3d013f7f775a9dc6a6bd37919ccf17
miyamoto <smiyamoto.610@gmail.com>
2025-09-06 11:54:54 +0900

    chore: riverpod_lint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:galleria_ai_hackathon_2025/presentation/app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ProviderScope(child: const MainApp()));
 }
